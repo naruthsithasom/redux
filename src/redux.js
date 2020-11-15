c6a64e5bcd8f8df377c5bd021287b33e57a1024a
naruthsithasom <ett0088@gmail.com>
2020-11-15 20:41:56 +0700

    redux

---
+++ src/redux.js
@@ -0,0 +1,30 @@
+const redux = require('redux')
+
+const initialState = {
+  counter : 0,
+}//Reducer
+const reducer = (state = initialState, action) => {
+  if(action.type === 'ADD_COUNTER'){
+    return {...state, counter: state.counter + action.value}
+  }
+if(action.type === 'INCRESE_COUNTER'){
+  return {...state, counter: state.counter + 1}
+}
+
+  return state
+}
+//Store
+const store = redux.createStore(reducer)
+console.log('1: ',store.getState())
+
+//Action
+action = {
+  type: "ADD_COUNTER",
+  value: 5,
+}
+store.dispatch({type:'INCRESE_COUNTER'})
+console.log('2: ',store.getState())
+store.dispatch(action)
+console.log('3: ',store.getState())
+
+//Subscriptionj
